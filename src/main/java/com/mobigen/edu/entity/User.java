56f097913b89791ae6941cef26ad9f4f690b16a1
hana kim <oneliebe@mobigen.com>
2025-02-18 20:17:12 +0900

    first

--- src/main/java/com/mobigen/edu/entity/User.java
+++ src/main/java/com/mobigen/edu/entity/User.java
@@ -1,13 +1,9 @@
 package com.mobigen.edu.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
